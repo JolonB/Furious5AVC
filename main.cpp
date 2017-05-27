1dcc761564026fd3d283522993015a09426568be
Jolon Behrent <jolon.behrent@gmail.com>
2017-05-27 17:32:08 +1200

    Added turning code

--- main.cpp
+++ main.cpp
@@ -1,45 +1,89 @@
+/*
+ * 
+ * I don't know why this code doesn't work. When it reaches the end of the line and detects all zeros
+ * it decides that it's time to quit. It will only detect the number of white pixels as zero once it
+ * detects all zero once. Then it will run the turning on the spot code to turn 90 degrees anticlockwise
+ * in order to try find a line. However, due to the bug, it will never detect the line and will just
+ * continue turning. The reason it doesn't work properly is because it returns to the colourCamera()
+ * function but doesn't run the camera (I honestly have no idea why), so it never runs it again. If you can figure
+ * out why that it happening and fix it, that would be great. The just upload it to GitHub and I'll try
+ * add it to the code we have already. 
+ * 
+ * 
+ * EDIT: i think i fixed it... maybe. Feel free to test it and see. Unfortuantely, it will juat do some rather 
+ * jerky turns on the spot until it reaches an angle where it can see everything.
+ * 
+ */
+
 #include <stdio.h>
 #include <time.h>
 #include "E101.h"
 
+float turn90 = 50; // adjust this value to change how fast it turns for
+float turnSleepS = 0;
+float turnSleepMs = 700000;
 float kp =0.3; // figure out a good value. 0.5 kind of worked
 float ki = 0;
 float kd = 0;
 float sp = 50;
+bool endOfLineTestOff = true;
 bool debug = false; // set to true to debug
 
-int motor(int err, int x){
+
+/*int turnAC(){ // Turn 90 degrees anti-clockwise
+	set_motor(1,-turn90);
+	set_motor(2,turn90);
+	sleep1(turnSleepS,turnSleepMs);
+	set_motor(1,0);
+	set_motor(2,0);
+	sleep1(2,0);
+	return 0;
+}
+
+void turnC(){ // Turn 90 degrees clockwise
+	set_motor(1,turn90);
+	set_motor(2,-turn90);
+	sleep1(turnSleepS,turnSleepMs);
+	set_motor(1,0);
+	set_motor(2,0);
+	return;	
+}*/
+
+void motor(int err, int x){
 	int speed = 100*err/12880;
-	if(x<10){
-	set_motor(1, sp);
-	set_motor(2, sp);
-//	sleep1(0,500);
+	if(x<10 && x>0){
+		set_motor(1, 2*sp);
+		set_motor(2, sp);
+		//sleep1(0,500);
+	}
+	else if(x==0){
+		set_motor(1,0);
+		set_motor(2,0);
+		set_motor(1,-turn90);
+	set_motor(2,turn90);
+	/*sleep1(turnSleepS,turnSleepMs);
+	set_motor(1,0);
+	set_motor(2,0);
+	sleep1(2,0);
+		//return 0;*/ //For some reason, this code is messing everything up. We dont need it, but it you fix it, our robot might look cooler.
+		printf("end\n");
 	}
 	else if(err>=0){
-	set_motor(2,-(sp+speed)); //left motor
-	set_motor(1,-(sp-2*speed)); //right motor
+		set_motor(2,-(sp+speed)); //left motor
+		set_motor(1,-(sp-2*speed)); //right motor
 	}
 	else if(err<0){
-	set_motor(2,-(sp+2*speed));
-	set_motor(1,-(sp-speed));
+		set_motor(2,-(sp+2*speed));
+		set_motor(1,-(sp-speed));
 	}
-	return 0;
+	//set_motor(1,0);
+	//set_motor(2,0);
+	//printf("done\n");
 }
-
-int gateOpener(){
-	char address[15] = {'1','3','0','.','1','9','5','.','6','.','1','9','6'};
-	connect_to_server(address, 1024);
-	char send[24] = {'P','l','e','a','s','e'};
-	send_to_server(send);
-	char pass[24];
-	receive_from_server(pass);
-	send_to_server(pass);
-	return 0;
-}
-
 
 int colourCamera(){
 	int threshold = 100;
+	//printf("Camera Started.");
 	while(true){
 		take_picture();
 		//int sumThreshold = 0;
@@ -64,6 +108,7 @@ int colourCamera(){
 			//sumThreshold = sumThreshold+white;
 			error = error+whiteBool*(i-160);
 			double p_signal = error*kp;
+			printf("%d\n", numWhite);
 			motor(p_signal, numWhite);
 		}
 		//threshold = sumThreshold/320;
@@ -76,6 +121,5 @@ return 0;
 
 int main(){
 	init();
-	gateOpener();
 	colourCamera();
 }
